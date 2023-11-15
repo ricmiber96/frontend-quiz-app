a4882e0e533bd835704d145cd7dde20355ef6e94
Ricardo Miralles <ricmiber@gmail.com>
2023-11-15 20:38:34 +0100

    Added Link on Header Icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ToogleButton from './ToggleButton'
 import useQuiz from '../../hooks/useQuiz'
 import { BACKGROUND_COLORS, QUIZES_ICONS } from '../../utils/quizzes'
+import { Link } from 'react-router-dom'
 
 export default function Header (props) {
   const { theme, quizCategory } = useQuiz()
@@ -12,7 +13,9 @@ export default function Header (props) {
     <header className='z-[2] flex items-center justify-between'>
       <div className={`${quizCategory ? 'opacity-100' : 'opacity-0'} flex items-center gap-6`}>
         <div className='p-1 bg-white rounded-md' style={bgStyle}>
-          {QUIZES_ICONS[quizCategory]}
+          <Link to='/'>
+            {QUIZES_ICONS[quizCategory]}
+          </Link>
         </div>
         <p className={`${theme ? 'text-white' : 'text-gray-600'} text-3xl font-medium`}>
           {quizCategory.toUpperCase()}
